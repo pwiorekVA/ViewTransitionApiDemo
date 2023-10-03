4212d2262d4b7d554e35cadb3cb20308a4a111b8
pwiorekVA <piotr.wiorek@valueadd.pl>
2023-10-03 22:14:08 +0200

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DogComponent } from "./dog/dog.component";
+import { CatComponent } from "./cat/cat.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dog',
+    component: DogComponent
+  },
+  {
+    path: 'cat',
+    component: CatComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/dog'
+  }
+];
---
+++ src/app/dog/dog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dog.component.html',
+  styleUrls: ['dog.component.css']
+})
+export class DogComponent {
+
+}
